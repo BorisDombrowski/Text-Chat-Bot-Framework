76292e6daa102ef20bcca59be8551fddb63f232d
BorisDombrowski <32637303+BorisDombrowski@users.noreply.github.com>
2022-05-07 00:47:09 +1000

    Middleware with bot name check
    + added a middleware, that checking is command from group chat starts by bot name

---
+++ ChatBotFramework/ChatBot/CommandValidationMiddleware/BotNameValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.CommandValidationMiddleware
+{
+    internal class BotNameValidator
+    {
+        private string[] _botNames;
+
+        public BotNameValidator(string[] botNames)
+        {
+            _botNames = botNames;
+        }
+
+        public bool IsBotName(string inputString)
+        {
+            return _botNames.Contains(inputString);
+        }
+    }
+}
---
+++ ChatBotFramework/ChatBot/CommandValidationMiddleware/IsCommandContainsBotNameMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using ChatBotFramework.Abstract;
+
+namespace ChatBot.CommandValidationMiddleware
+{
+    internal class IsCommandContainsBotNameMiddleware : CommandValidationMiddlewareBase
+    {
+        private BotNameValidator _botNameValidator;
+
+        public IsCommandContainsBotNameMiddleware(ICommandExecutor executor, BotNameValidator botNameValidator) 
+            : base(executor) 
+        {
+            _botNameValidator = botNameValidator;
+        }
+
+        public override void OnCommandReceived(CommandDataBase commandData)
+        {
+            var command = commandData.Command.Trim();
+            var startsByBotName = IsCommandStartsByBotName(command);
+
+            if (commandData.IsCommandFromGroupChat() && !startsByBotName)
+            {
+                return;
+            }
+
+            if (startsByBotName)
+            {
+                command = RemoveBotNameFromCommand(command);
+            }
+
+            var commandResult = SendCommandForExecution(command);
+            commandData.SendResponse(commandResult);
+        }
+
+        private bool IsCommandStartsByBotName(string command)
+        {
+            var split = command.Split(' ');
+            return _botNameValidator.IsBotName(split[0]);
+        }
+
+        private string RemoveBotNameFromCommand(string command)
+        {
+            var split = command.Split(' ').ToList();
+            split.RemoveAt(0);
+            return string.Join(' ', split);
+        }
+    }
+}
